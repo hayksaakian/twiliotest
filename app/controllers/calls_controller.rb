5bd46dcfdaaa931811499f4befc45dd8e7b74cc3
hayksaakian <fakest.id.ever@gmail.com>
2012-09-07 19:25:12 -0700

    whatevr

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,4 +1,18 @@
 class CallsController < ApplicationController
+  def create
+  	response = Twilio::TwiML::Response.new do |r|
+		  r.Say 'hello there', :voice => 'woman'
+		  # r.Dial :callerId => '+12067455752' do |d|
+		  #   d.Client 'jenny'
+		  # end
+		end
+		# print the result
+		@r = response.text
+    respond_to do |format|
+      format.html {}
+      format.xml {render :inline => @r}
+    end
+  end
   def index
   	response = Twilio::TwiML::Response.new do |r|
 		  r.Say 'hello there', :voice => 'woman'
